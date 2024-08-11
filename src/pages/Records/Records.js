340fca544126c5178786f23d40c692a21857f383
unknown <dickensallinone@gmail.com>
2024-08-11 17:33:20 +0800

    fix records design

--- src/pages/Records/Records.js
+++ src/pages/Records/Records.js
@@ -1,13 +1,38 @@
 import React from "react";
 import BodyComponent from "../../components/BodyComponent/BodyComponent";
+import { motion } from "framer-motion";
+import records from "./Records.module.css";
 const Records = ({ data }) => {
+
+    const container = {
+        hidden: { opacity: 1, scale: 0 },
+        visible: {
+          opacity: 1,
+          scale: 1,
+          transition: {
+            delayChildren: 0.3,
+            staggerChildren: 0.2
+          }
+        }
+      };
+      
+      const item = {
+        hidden: { y: 20, opacity: 0 },
+        visible: {
+          y: 0,
+          opacity: 1
+        }
+      };
 
     return (
         <BodyComponent>
-
-            <ul>
+            <motion.ul 
+            variants={container}
+            initial="hidden"
+            animate="visible"
+            className={records['records-container']}>
                 {data?.map(val => (
-                    <li key={val.idPk}>
+                    <motion.li key={val.idPk} variants={item}>
                         <div>Quiz Id: {val.quizUUID} </div>
                         <div> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pause-circle" viewBox="0 0 16 16">
                             <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
@@ -21,9 +46,9 @@ const Records = ({ data }) => {
                             <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
                             <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
                         </svg> Incorrect Answers: {val.totalIncorrect}</div>
-                    </li>
+                    </motion.li>
                 ))}
-            </ul>
+            </motion.ul>
         </BodyComponent>
     )
 }
